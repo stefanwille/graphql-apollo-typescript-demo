e080c68c7d6a17a4d6e6c165f073f3bd4d597f11
Stefan Wille <github@stefanwille.com>
2018-08-17 17:31:53 +0200

    Be more TypeScript idiomatic

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,6 +1,6 @@
-class User {
-  public firstName = "";
-  public lastName = "";
+interface User {
+  firstName: string;
+  lastName: string;
 }
 
 const resolvers = {
@@ -11,21 +11,21 @@ const resolvers = {
     me(obj: object, args: {}) {
       return { firstName: "Gregor", lastName: "Ganss" };
     },
-    userById(obj: object, args: { id: string }) : User {
+    userById(obj: object, args: { id: string }): User {
       return { firstName: "Hector " + args.id, lastName: "Identy" };
-    }
+    },
   },
   Mutation: {
-    updateUser(obj: object, args: |{ id: string }) : User{
+    updateUser(obj: object, args: { id: string }): User {
       console.log("updateUser", args);
       return { firstName: "Esteban " + args.id, lastName: "Mutación" };
-    }
+    },
   },
   User: {
     firstName(obj: User, args: {}) {
       return obj.firstName;
-    }
-  }
+    },
+  },
 };
 
 export default resolvers;
